1fa6ac10082ad7782b4acfb04992286d5247c3d8
Ambareesh Vishwanath <c.ambareesh@hotmail.com>
2022-01-02 10:46:36 +0530

    Initial commit

--- BookStore/Models/ErrorViewModel.cs
+++ BookStore/Models/ErrorViewModel.cs
@@ -4,6 +4,7 @@ namespace BookStore.Models
 {
     public class ErrorViewModel
     {
+
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
